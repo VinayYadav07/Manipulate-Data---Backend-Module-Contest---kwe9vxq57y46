e38db12e5ab835dfa1389d4f1ba5e698dd0bf22c
VinayYadav07 <98084463+VinayYadav07@users.noreply.github.com>
2023-09-17 19:29:42 +0530

    src/index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,55 @@ const resources = JSON.parse(fs.readFileSync(`${__dirname}/data/resources.json`)
 dotenv.config();
 const app = express();
 
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Endpoint to get all resources
+app.get("/resources", (req, res) => {
+  res.status(200).json(resources);
+});
 
+// Endpoint to filter resources by category
+app.get("/resources", (req, res) => {
+  const { category } = req.query;
+  if (category) {
+    const filteredResources = resources.filter(
+      (resource) => resource.category === category
+    );
+    res.status(200).json(filteredResources);
+  } else {
+    res.status(200).json(resources);
+  }
+});
+
+// Endpoint to get sorted resources by name
+app.get("/resources/sort", (req, res) => {
+  const { sortBy } = req.query;
+  if (sortBy === "name") {
+    const sortedResources = [...resources].sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    res.status(200).json(sortedResources);
+  } else {
+    res.status(200).json(resources);
+  }
+});
+
+// Endpoint to group resources by category
+app.get("/resources/group", (req, res) => {
+  const groupedResources = resources.reduce((acc, resource) => {
+    if (!acc[resource.category]) {
+      acc[resource.category] = [];
+    }
+    acc[resource.category].push(resource);
+    return acc;
+  }, {});
+  res.status(200).json(groupedResources);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = { app, server }; // Export both app and server
